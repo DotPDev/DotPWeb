38953ef4e714c335a3d3bc7166115ac2fcdcfa0e
kennedyb <brian.kennedy@wwt.com>
2017-02-09 09:31:02 -0600

    adding all new files

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -16,13 +16,14 @@ module.exports = function (grunt) {
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
-    cdnify: 'grunt-google-cdn'
+    cdnify: 'grunt-google-cdn',
+    configureProxies: 'grunt-connect-proxy'
   });
 
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: 'dist'
+    dist: '../server/dist' // <-- MODIFY
   };
 
   // Define the configuration for all the tasks
@@ -75,11 +76,19 @@ module.exports = function (grunt) {
         hostname: 'localhost',
         livereload: 35729
       },
+      proxies: [
+        {
+          context: '/api',
+          host: 'localhost',
+          port: 3000
+        }
+      ],
       livereload: {
         options: {
           open: true,
           middleware: function (connect) {
             return [
+              require('grunt-connect-proxy/lib/utils').proxyRequest,
               connect.static('.tmp'),
               connect().use(
                 '/bower_components',
@@ -167,6 +176,9 @@ module.exports = function (grunt) {
           ]
         }]
       },
+      options: {
+        force: true
+      },
       server: '.tmp'
     },
 
@@ -220,7 +232,7 @@ module.exports = function (grunt) {
             }
           }
       }
-    }, 
+    },
 
     // Renames files for browser caching purposes
     filerev: {
@@ -431,6 +443,7 @@ module.exports = function (grunt) {
       'clean:server',
       'wiredep',
       'concurrent:server',
+      'configureProxies',
       'postcss:server',
       'connect:livereload',
       'watch'
