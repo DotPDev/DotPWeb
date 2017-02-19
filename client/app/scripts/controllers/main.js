961b0c7a81f072d2cd1993fe7daaaf939bec9b00
Lindemann <alex.lindemann@wwt.com>
2017-02-18 18:08:25 -0600

    basic pagination links with hacky workaround :(

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -11,18 +11,59 @@ angular.module('clientApp')
 .controller('MainCtrl', function ($http, feedManager, utils) {
     var vm = this;
     vm.feed = {};
+    vm.page = 1;
+    vm.links = {
+        next: "/",
+        goNext: goNext,
+        prev: "/",
+        goPrev: goPrev
+    };
 
     function init() {
-        var page = parseInt(utils.getParameterByName('page'));
+        console.log("test");
+        vm.page = parseInt(utils.getParameterByName('page'));
 
-        if (!page) {
-            page = 1;
+        if (!vm.page) {
+            vm.page = 1;
         }
-        getFeed(page);
+        setPageLinks(vm.page);
+        getFeed(vm.page);
     }
 
-    function getFeed(page) {
-        feedManager.parseFeed(page).then(function(response) {
+    function setPageLinks() {
+        console.log("setPages - " + vm.page);
+        if (vm.page === 1) {
+            vm.links.next = "/?page=" + (vm.page + 1);
+            vm.links.prev = "/";
+        } else {
+            vm.links.next = "/?page=" + (vm.page + 1);
+            vm.links.prev = "/?page=" + (vm.page - 1);
+        }
+    }
+
+    function goNext() {
+        setTimeout(function() {
+            console.log("page " + vm.page);
+            vm.page += 1;
+            setPageLinks();
+            getFeed();
+            console.log("page2 " + vm.page);
+        },1);
+
+    }
+
+    function goPrev() {
+        setTimeout(function() {
+            if (vm.page > 1) {
+                vm.page -= 1;
+                setPageLinks();
+                getFeed();
+            }
+        },1);
+    }
+
+    function getFeed() {
+        feedManager.parseFeed(vm.page).then(function(response) {
             console.log(response);
             vm.feed = response;
         }).catch(function(error) {
