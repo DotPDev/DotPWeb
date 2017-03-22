56724166d2461e1ea6076d43c4f9341ad21dd094
Lindemann <alex.lindemann@wwt.com>
2017-03-21 23:25:23 -0500

    super ultra basic store UI

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -106,6 +106,20 @@ angular
             }
         }
     });
+    $stateProvider.state('root.store', {
+        url: '/store',
+        data: {
+            pageName: 'StoreCtrl',
+            browserTitle: 'Store'
+        },
+        views: {
+            'container@': {
+                templateUrl: 'views/store.html',
+                controller: 'StoreCtrl',
+                controllerAs: 'vm'
+            }
+        }
+    });
     $stateProvider.state('root.login', {
         url: '/login',
         resolve: {
