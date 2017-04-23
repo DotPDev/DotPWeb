db316c4bd6e8c52be63f18bd0804e494cf97ec7c
Lindemann <alex.lindemann@wwt.com>
2017-04-23 17:03:11 -0500

    pagination containers on about

--- client/app/scripts/controllers/about.js
+++ client/app/scripts/controllers/about.js
@@ -8,10 +8,29 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('AboutCtrl', function () {
+  .controller('AboutCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    var vm = this;
+    vm.selectedSection = 'dotp';
+
+    function navigateAbout(str) {
+      console.log(str);
+      vm.selectedSection = str;
+    }
+
+    function getActiveState(str) {
+      if (str == vm.selectedSection) {
+        return 'active'
+      }
+    }
+
+    return {
+      getActiveState: getActiveState,
+      navigateAbout: navigateAbout
+    }
   });
