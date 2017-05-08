3ff48b23bc09f55357542e8456873b66e1a8b8ee
Lindemann <alex.lindemann@wwt.com>
2017-05-08 17:40:58 -0500

    minor changes

--- client/app/scripts/controllers/order-status.js
+++ client/app/scripts/controllers/order-status.js
@@ -17,27 +17,26 @@ angular.module('clientApp')
     var vm = this;
 
     vm.orderId = $stateParams.orderId
-    vm.printfulStatus = "Pending";
+    vm.printfulStatus = "Retrieving status";
     vm.shippingStatus = "Pending";
     vm.shippingLink = null;
 
     function getOrderStatus(orderId) {
-      return new Promise(function (fulfill, reject){
-          $http.get('/api/printful/orders/' + vm.orderId).then(function(response) {
-            var order = response.data;
-            console.log(response.data);
-            fulfill(response.data);
-            vm.printfulStatus = order.status;
-            if (order.shipments.length > 0){
-              vm.shippingStatus = order.shipments[0].service + " - " + order.shipments[0].tracking_number;
-              vm.shippingLink = order.shipments[0].tracking_url;
-            }
-          }).catch(function(error) {
-            //TODO show UI error to user
-            console.log(error);
-            reject(error)
-          });
-      })
+      console.log("Getting status");
+      $http.get('/api/printful/orders/' + vm.orderId).then(function(response) {
+        var order = response.data;
+        console.log(response.data);
+        vm.printfulStatus = order.status;
+        if (order.shipments.length > 0){
+          vm.shippingStatus = order.shipments[0].service + " - " + order.shipments[0].tracking_number;
+          vm.shippingLink = order.shipments[0].tracking_url;
+        }
+      }).catch(function(error) {
+        //TODO show UI error to user
+        vm.printfulStatus = "Error Getting Status";
+        vm.shippingStatus = "Error Getting Status"
+        console.log(error);
+      });
     }
 
     getOrderStatus(vm.orderId);
